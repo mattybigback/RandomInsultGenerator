6747a1915aa0278a6a50dca25fb127138ec32587
Matt Harrison <6126189+mattybigback@users.noreply.github.com>
2024-11-16 21:45:04 +0000

    New splash messages

--- include/SplashMessages.hpp
+++ include/SplashMessages.hpp
@@ -3,21 +3,20 @@
 
 #include "main.hpp"
 
-const char splashMessage_00[] PROGMEM = "Don't press my buttons if you can't handle the banter";
-const char splashMessage_01[] PROGMEM = "Not suitable for children";
-const char splashMessage_02[] PROGMEM = "For when you have too much self esteem";
+const char splashMessage_00[] PROGMEM = "Don't press my buttons if you can't handle the banter.";
+const char splashMessage_01[] PROGMEM = "Not suitable for children or HR managers.";
+const char splashMessage_02[] PROGMEM = "For when you have too much self esteem.";
 const char splashMessage_03[] PROGMEM = "Do you really want to do this?";
 const char splashMessage_04[] PROGMEM = "This won't make you feel good about yourself.";
-
-
-
+const char splashMessage_05[] PROGMEM = "I've been known to make grown men cry";
 
 const char *const splashMessage_table[] PROGMEM = {
                                         splashMessage_00,
                                         splashMessage_01,
                                         splashMessage_02,
                                         splashMessage_03,
-                                        splashMessage_04
+                                        splashMessage_04,
+                                        splashMessage_05
                                         };
 
 const uint8_t splashCount = sizeof(splashMessage_table) / sizeof(splashMessage_table[0]);
